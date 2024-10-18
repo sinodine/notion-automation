5692c2642295c836a202946ca4b8f61c74a529ef
Sixtine Nodet <sixtine.nodet@gmail.com>
2024-10-19 01:49:22 +0200

    Add finish comment management

--- constants.example.py
+++ constants.example.py
@@ -1,7 +1,6 @@
 NOTION_API_TOKEN = "your-integration-api-token"
 DATABASE_ID = "your-notion-page-id"
 
-
 # Sample result dictionary (simulating the API response)
 properties = {
     'Date d’échéance': {'id': 'C%5C%3C%3A', 'type': 'date', 'date': {'start': '2022-12-19', 'end': None, 'time_zone': None}},
@@ -11,23 +10,15 @@ properties = {
     'Nom': {'id': 'title', 'type': 'title', 'title': [{'type': 'text', 'text': {'content': 'Réfléchir à des idées de fonctionnalités', 'link': None}, 'plain_text': 'Réfléchir à des idées de fonctionnalités'}]}
 }
 
+# URLs
+NOTION_API_URL = "https://api.notion.com/v1"
+DATABASE_URL = f"{NOTION_API_URL}/databases/{DATABASE_ID}"
+PAGE_URL_TEMPLATE = f"{NOTION_API_URL}/pages/{{page_id}}"
+BLOCK_URL_TEMPLATE = f"{NOTION_API_URL}/blocks/{{page_id}}/children"
+USER_URL_TEMPLATE = f"{NOTION_API_URL}/users/{{user_id}}"
 
-API_BASE_URL = "https://api.notion.com/v1"
-RETRIEVE_BLOCK_URL = f"{API_BASE_URL}/blocks/{DATABASE_ID}/children" # GET: To retrieve the paginated blocks of a page (max 100 blocks) # POST: To create a new block # PATCH: To create and append a blocks
-RETRIEVE_PAGE_URL = f"{API_BASE_URL}/pages/{DATABASE_ID}"
-RETRIEVE_DATABASE_URL = f"{API_BASE_URL}/databases/{DATABASE_ID}" # GET: To retrieve a database columns
-GET_BLOCK_URL = f"{API_BASE_URL}/blocks/{{block_id}}" # GET: To retrieve a block # PATCH: To update a block # DELETE: To delete a block
-QUERY_DATABASE_URL = f"{API_BASE_URL}/databases/{{database_id}}/query" # POST to query the database with filters
-RETRIEVE_USER_URL = f"{API_BASE_URL}/users/me" # GET to retrieve the bot token user
-COMMENTS_URL = f"{API_BASE_URL}/comments" # POST to create a comment block_id and content # GET to retrieve all comments of a block_id
-
-
-# One data base -> multiple pages
-# One page -> multiple blocks
-
-# properties = columns
-# page = item (row)
-# block = content
-
-
-# pages are technically blocks
+# For tests
+MY_USER_ID = "your-user-id" # To test the mention feature
+PAGE_URL1 = "https://www.notion.so/Notion-Test-Page-1-122acdc2cc5880fa8643c616c6778bbb"
+PAGE_URL2 = "https://www.notion.so/Notion-Test-Page-2-122acdc2cc5880fa8643c616c6778bbb"
+PAGE_URL3 = "https://www.notion.so/Notion-Test-Page-3-122acdc2cc5880fa8643c616c6778bbb"
